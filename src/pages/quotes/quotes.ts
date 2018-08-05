0185016c6d8a1594619a90d7aed4c5c30e6d6626
Abraham Matus <aimatuscogmail.com>
2018-08-05 18:57:17 -0500

    Adds quotes page and pass proper quotes.

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,25 +1,19 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the QuotesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Component, OnInit } from '@angular/core';
+import { IonicPage, NavParams } from 'ionic-angular';
+import { Quote } from '../../data/quote.interface';
 
 @IonicPage()
 @Component({
   selector: 'page-quotes',
   templateUrl: 'quotes.html',
 })
-export class QuotesPage {
+export class QuotesPage implements OnInit{
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  quoteGroup: { category: string, quotes: Quote[], icon: string };
+
+  constructor(private navParams: NavParams) { }
+
+  ngOnInit(): void {
+    this.quoteGroup = this.navParams.data;
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotesPage');
-  }
-
 }
