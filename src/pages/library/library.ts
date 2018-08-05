1abd2278101e454efad23ada8e525470aa055094
Abraham Matus <aimatusco@gmail.com>
2018-08-05 18:57:17 -0500

    Adds quotes page and pass proper quotes.

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular';
 import { Quote } from '../../data/quote.interface';
+import { QuotesPage } from '../quotes/quotes';
 import quotes from '../../data/quotes';
 
 @IonicPage()
@@ -11,6 +12,7 @@ import quotes from '../../data/quotes';
 export class LibraryPage implements OnInit {
 
   quoteCollection: { category: string, quotes: Quote[], icon: string }[];
+  quotesPage = QuotesPage;
 
   ngOnInit(): void {
     this.quoteCollection = quotes;
