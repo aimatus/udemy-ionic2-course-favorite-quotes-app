237dcb3bb5e33bac452070bce045548248f8d1fc
Abraham Matus <aimatusco@gmail.com>
2018-08-05 19:22:45 -0500

    Adds modal for adding quote to favorites.

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavParams } from 'ionic-angular';
+import { IonicPage, NavParams, AlertController } from 'ionic-angular';
 import { Quote } from '../../data/quote.interface';
 
 @IonicPage()
@@ -11,9 +11,35 @@ export class QuotesPage implements OnInit{
 
   quoteGroup: { category: string, quotes: Quote[], icon: string };
 
-  constructor(private navParams: NavParams) { }
+  constructor(
+    private navParams: NavParams,
+    private alertController: AlertController) { }
 
   ngOnInit(): void {
     this.quoteGroup = this.navParams.data;
+  }
+
+  onAddToFavorite(selectedQuote: Quote) {
+    const alert = this.alertController.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to add the quote?',
+      buttons: [
+        {
+          text: 'Yes, go ahead',
+          handler: () => {
+            console.log('Ok');
+          }
+        },
+        {
+          text: 'No, I changed my mind',
+          role: 'cancel',
+          handler: () => {
+            console.log('Canceled');
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 }
