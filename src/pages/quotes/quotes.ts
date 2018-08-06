0913f9b256f802c917777abba2ed5d0f62096316
Abraham Matus <aimatuscogmail.com>
2018-08-05 19:26:34 -0500

    Refactors add to favorite modal buttons code.

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -7,7 +7,7 @@ import { Quote } from '../../data/quote.interface';
   selector: 'page-quotes',
   templateUrl: 'quotes.html',
 })
-export class QuotesPage implements OnInit{
+export class QuotesPage implements OnInit {
 
   quoteGroup: { category: string, quotes: Quote[], icon: string };
 
@@ -24,22 +24,23 @@ export class QuotesPage implements OnInit{
       title: 'Add Quote',
       subTitle: 'Are you sure?',
       message: 'Are you sure you want to add the quote?',
-      buttons: [
-        {
-          text: 'Yes, go ahead',
-          handler: () => {
-            console.log('Ok');
-          }
-        },
-        {
-          text: 'No, I changed my mind',
-          role: 'cancel',
-          handler: () => {
-            console.log('Canceled');
-          }
-        }
-      ]
+      buttons: [this.yesButton, this.cancelButton]
     });
     alert.present();
   }
+
+  yesButton = {
+    text: 'Yes, go ahead',
+    handler: () => {
+      console.log('Ok');
+    }
+  };
+
+  cancelButton = {
+    text: 'No, I changed my mind',
+    role: 'cancel',
+    handler: () => {
+      console.log('Canceled');
+    }
+  };
 }
